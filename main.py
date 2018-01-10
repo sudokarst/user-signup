910c78b4589ec6e774992b9b3349ff8ae4700720
sudokarst <sudokarst@gmail.com>
2018-01-09 19:30:01 -0500

    fixed bug where username did not stay in field on index.html

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 """
+Requirements:
+
+Error on
+
 The user leaves any of the following fields empty: username, password, verify password.
 The user's username or password is not valid -- for example, it contains a space character or it consists of less than 3 characters or more than 20 characters (e.g., a username or password of "me" would be invalid).
 The user's password and password-confirmation do not match.
 The user provides an email, but it's not a valid email. Note: the email field may be left empty, but if there is content in it, then it must be validated. The criteria for a valid email address in this assignment are that it has a single @, a single ., contains no spaces, and is between 3 and 20 characters long.
+
+For the username and email fields, you should preserve what the user typed, so they don't have to retype it. With the password fields, you should clear them, for security reasons.
 """
 from flask import Flask, redirect, request, render_template
 
@@ -64,7 +70,7 @@ def validate():
     if validated:
         return redirect("/welcome")
     else:
-        return render_template("index.html", **locals())
+        return render_template("index.html", username=username, **locals())
 
 @app.route("/welcome")
 def welcome():
